4115ba366c0ce5b3f7018a3059d4ac44df9ee178
arnoldc94 <carnold9438@gmail.com>
2022-07-21 11:50:30 -0700

    progress

--- app/controllers/blog_post_controller.rb
+++ app/controllers/blog_post_controller.rb
@@ -6,7 +6,22 @@ class BlogPostController < ApplicationController
         @blog = BlogPost.find(params[:id])
     end
     def new
-        
+        # @blog = BlogPost.new
+    end
+    def create
+        @blog = BlogPost.create(blog_params)
+        if @blog.valid?
+            redirect_to blogs_path
+        else
+            redirect_to new_blog_path
+        end
+    end
+      
+    private
+    def blog_params
+    params.require(:blog_post).permit(:title, :content)
     end
     
+      
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   get 'blogs' => 'blog_post#index', as: 'blogs'
-  get 'blog/:id' => 'blog_post#show', as:'blog'
-  get 'blogs/new' => 'blog_post#new', as: 'new_post'
+  get 'blogs/new' => 'blog_post#new', as: 'new_blog'
+  post 'blogs' => 'blog_post#create'
+  get 'blogs/:id' => 'blog_post#show', as:'blog'
   root 'blog_post#index'
 
 end
